c5c4c15179cc396eeeba00d4f3d0541541bc41f0
sananta <sananta@ucdavis.edu>
2019-08-02 17:18:29 -0700

    I am successfully rich...

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Srivarshini Ananta on 7/30/19.
+//  Copyright © 2019 Varshini Ananta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
